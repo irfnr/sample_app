d41ddb545eb9493730f0ec802c1bbb92f21b2ee6
irfnr <irfan.rasool@cuelogic.co.in>
2014-02-25 11:41:07 +0530

    adding the new pages and new code from learning rails

---
+++ example_user.rb
@@ -0,0 +1,16 @@
+
+class User
+
+	attr accessor :name, :email
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:email]
+	end
+
+	
+	def formatted email
+		"#{@name} <#{@email}>"
+	end
+
+end
